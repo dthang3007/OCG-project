f19cce1bc4fd36ee9ceb7ec0777e8ad6c9bc4f7f
pham thang <pdthang.cva@gmail.com>
2021-07-07 12:26:40 +0700

    add-frontent=theme

--- Melody-shop-frontend/src/axios.js
+++ Melody-shop-frontend/src/axios.js
@@ -1,10 +1,10 @@
+import axios from "axios";
+const rootPath = "http://localhost:3000/api";
 
-import axios from 'axios';
-const rootPath="http://localhost:3000/api"
 const axiosInstance = axios.create({
-    baseURL: rootPath,
+  baseURL: rootPath,
 });
 axiosInstance.interceptors.request.use(
-    (config) => new Promise((resolve) => setTimeout(() =>  resolve(config), 2000 ))
-)
-export default axiosInstance
+  (config) => new Promise((resolve) => setTimeout(() => resolve(config), 2000))
+);
+export default axiosInstance;
